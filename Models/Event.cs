f0feecf5217dcbd49a26d1ab1f92e41e9dad3fcf
Ivan <a2021116557@estudiantes.upsa.edu.bo>
2025-09-29 20:10:45 -0400

    codigo para el models

--- Models/Event.cs
+++ Models/Event.cs
@@ -2,5 +2,11 @@
 
 public class Event
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Location { get; set; } = null!;
+    public DateTime StartAt { get; set; }
+    public DateTime? EndAt { get; set; }     // nullable
+    public bool IsOnline { get; set; }
+    public string? Notes { get; set; }       // nullable
 }
